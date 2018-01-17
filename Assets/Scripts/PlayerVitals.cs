2f043f66b1c42a3dd681193a1ba91eb5169e746f
Twistedziefer <shawnantonucci2017@gmail.com>
2018-01-17 10:07:16 -0800

    First Commit
    Version 0.1

---
+++ Assets/Scripts/PlayerVitals.cs
@@ -0,0 +1,174 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson; //NEW
+
+public class PlayerVitals : MonoBehaviour
+{
+    public Slider healthSlider;
+    public int maxHealth;
+    public int healthFallRate;
+
+    public Slider thirstSlider;
+    public int maxThirst;
+    public int thirstFallRate;
+
+    public Slider hungerSlider;
+    public int maxHunger;
+    public int hungerFallRate;
+
+    public Slider staminaSlider; //NEW
+    public int maxStamina; //NEW
+    private int staminaFallRate; //NEW
+    public int staminaFallMult; //NEW
+    private int staminaRegainRate; //NEW
+    public int staminaRegainMult; //NEW
+
+    [Header("Temperature Settings")]
+    public float freezingTemp;
+    public float currentTemp;
+    public float normalTemp;
+    public float heatTemp;
+    public Text tempNumber;
+    public Image tempBG;
+
+    private CharacterController charController; //NEW
+    private FirstPersonController playerController; //NEW
+
+    void Start()
+    {
+        charController = GetComponent<CharacterController>(); //NEW
+        playerController = GetComponent<FirstPersonController>(); //NEW
+
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = maxHealth;
+
+        thirstSlider.maxValue = maxThirst;
+        thirstSlider.value = maxThirst;
+
+        hungerSlider.maxValue = maxHunger;
+        hungerSlider.value = maxHunger;
+
+        staminaSlider.maxValue = maxStamina; //NEW
+        staminaSlider.value = maxStamina; //NEW
+
+        staminaFallRate = 1; //NEW
+        staminaRegainRate = 1; //NEW
+    }
+
+    void UpdateTemp()
+    {
+        tempNumber.text = currentTemp.ToString("00.0");
+    }
+
+    void Update()
+    {
+        //TEMPERATURE SECTION
+        if(currentTemp <= freezingTemp)
+        {
+            tempBG.color = Color.blue;
+            UpdateTemp();
+        }
+        else if (currentTemp >= heatTemp - 0.1)
+        {
+            tempBG.color = Color.red;
+            UpdateTemp();
+        }
+        else
+        {
+            tempBG.color = Color.green;
+            UpdateTemp();
+        }
+
+        //HEALTH CONTROLLER
+        if (hungerSlider.value <= 0 && (thirstSlider.value <= 0))
+        {
+            healthSlider.value -= Time.deltaTime / healthFallRate * 2;
+        }
+
+        else if(hungerSlider.value <= 0 || thirstSlider.value <= 0 || currentTemp <= freezingTemp || currentTemp >= heatTemp)
+        {
+            healthSlider.value -= Time.deltaTime / healthFallRate;
+        }
+
+        if (healthSlider.value <= 0)
+        {
+            CharacterDeath();
+        }
+
+        //HUNGER CONTROLLER
+        if (hungerSlider.value >= 0)
+        {
+            hungerSlider.value -= Time.deltaTime / hungerFallRate;
+        }
+
+        else if (hungerSlider.value <= 0)
+        {
+            hungerSlider.value = 0;
+        }
+
+        else if (hungerSlider.value >= maxHunger)
+        {
+            hungerSlider.value = maxHunger;
+        }
+
+        //THIRST CONTROLLER
+        if (thirstSlider.value >= 0)
+        {
+            thirstSlider.value -= Time.deltaTime / thirstFallRate;
+        }
+
+        else if (thirstSlider.value <= 0)
+        {
+            thirstSlider.value = 0;
+        }
+
+        else if (thirstSlider.value >= maxThirst)
+        {
+            thirstSlider.value = maxThirst;
+        }
+
+        //STAMINA CONTROL SECTION
+
+        if (charController.velocity.magnitude > 0 && Input.GetKey(KeyCode.LeftShift))
+        {
+            staminaSlider.value -= Time.deltaTime / staminaFallRate * staminaFallMult;
+
+            if(staminaSlider.value > 0)
+            {
+                currentTemp += Time.deltaTime / 5;
+            }
+        }
+
+        else
+        {
+            staminaSlider.value += Time.deltaTime / staminaRegainRate * staminaRegainMult;
+
+            if(currentTemp >= normalTemp)
+            {
+                currentTemp -= Time.deltaTime / 10;
+            }
+        }
+
+        if (staminaSlider.value >= maxStamina)
+        {
+            staminaSlider.value = maxStamina;
+        }
+
+        else if (staminaSlider.value <= 0)
+        {
+            staminaSlider.value = 0;
+            playerController.m_RunSpeed = playerController.m_WalkSpeed;
+        }
+
+        else if(staminaSlider.value >= 0)
+        {
+            playerController.m_RunSpeed = playerController.m_RunSpeedNorm;
+        }
+    }
+
+    void CharacterDeath()
+    {
+        //DO SOMETHING HERE!
+    }
+}
